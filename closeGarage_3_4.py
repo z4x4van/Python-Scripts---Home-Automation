9fafab4420ff8538ff80bf742c9e7b8f8a6c72f0
z4x4van <frank.gleason@gmail.com>
2016-01-22 12:21:28 -0600

    Close garage on GPIO 3 only if input pin 4 is open

---
+++ closeGarage_3_4.py
@@ -0,0 +1,17 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(3, GPIO.OUT)
+if GPIO.input(4):
+        print("Closing Garage Door")
+        GPIO.output(3,GPIO.LOW)
+        time.sleep(1)
+        GPIO.output(3,GPIO.HIGH)
+        GPIO.cleanup()
+else:
+        print("Already Closed")
+        GPIO.cleanup()
+
+
+
